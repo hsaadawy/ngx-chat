f810cda1b572e46901dfbe0fc31575e6c441bfce
hossam.saadawy640 <hossam.saadawy640@outlook.com>
2023-02-08 18:10:01 +0200

    upddat some steps

--- projects/pazznetwork/ngx-chat/src/lib/core/callService.ts
+++ projects/pazznetwork/ngx-chat/src/lib/core/callService.ts
@@ -34,7 +34,7 @@ export class CallService
                     if (!this.peerList.includes(call.peer)) {
                       debugger;
                         this.streamRemoteVideo(remoteStream);
-                        this.showLocalVideo();
+                       // this.showLocalVideo();
                       this.currentPeer = call.peerConnection;
                       this.peerList.push(call.peer);
 
@@ -77,7 +77,7 @@ export class CallService
       connectVideo(peerId:any): void {
         debugger;    
         this.callPeer(peerId);
-        this.showLocalVideo();
+        //this.showLocalVideo();
       }
       connectAudio(peerid:any): void {
         debugger;
@@ -206,5 +206,11 @@ export class CallService
         this.closeLocalVideo();
         this.peer.destroy();
       }
+      muteAudio()
+  {
+      this.lazyStream.getAudioTracks().forEach(function(track:MediaStreamTrack) {
+      track.enabled=!track.enabled;
+        });     
+  }
   
 }
